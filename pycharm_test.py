0aee1c72b8adb2fb9a9aa3f855e2ede06422e331
larsniemdik <niemczik@dik.tu-darmstadt.de>
2020-11-22 12:51:16 +0100

    Patching pydevd to work correctly with PyCharm
    Mechanism implemeted to fix Issues with unmappable files

--- pycharm_test.py
+++ pycharm_test.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 # AUTHOR Anyone
 # VERSION 1.0.0
-# Hello World for debugging with PyCharm
+# Hello World for debugging with PyCharm (2019.2.6)
 
-# Prepare environment
+################################################################################################
+# Preprare debugging
+################################################################################################
+import sys, os
 import pydevd
+
+root = os.path.dirname(__file__)
+sys.path.append(root)
+os.chdir(root)
+# Fix unmappable file/s issue
+pydevd.mapping_patches = {"<string>": os.path.basename(__file__)}
+
+################################################################################################
 
 # Debugger will stop here because suspend is set to true.
 pydevd.settrace('localhost', port=15678, stdoutToServer=True, stderrToServer=True, suspend=True)
